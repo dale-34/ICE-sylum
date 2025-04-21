7915eda9e86ca76da14a7bf3a8ac4a5d22d734fe
Ethan <ethanle480@gmail.com>
2025-04-21 11:54:52 -0400

    created npc mocks

---
+++ Assets/Scripts/LookCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCamera : MonoBehaviour
+{
+
+    public Transform m_Camera;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(new Vector3(m_Camera.position.x, transform.position.y, m_Camera.position.z));
+    }
+
+}
